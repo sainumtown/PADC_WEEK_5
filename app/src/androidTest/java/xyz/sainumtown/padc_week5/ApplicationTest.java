63233d0594448f662dec1292345ad6fc7ea7559d
Sai num town <sainumtown@gmail.com>
2016-07-08 14:38:50 +0630

    First commit

---
+++ app/src/androidTest/java/xyz/sainumtown/padc_week5/ApplicationTest.java
@@ -0,0 +1,13 @@
+package xyz.sainumtown.padc_week5;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
